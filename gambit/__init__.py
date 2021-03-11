c6abb5ff9d67262f80d64c193f3693221618ae15
Christoph Gote <gotec@me.com>
2021-03-11 19:08:40 +0100

    Update __init__.py

--- gambit/__init__.py
+++ gambit/__init__.py
@@ -4,6 +4,6 @@ An Open Source name disambiguation tool for version control systems.
 
 __author__ = "Christoph Gote"
 __email__ = "cgote@ethz.ch"
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 from .main import disambiguate_aliases
