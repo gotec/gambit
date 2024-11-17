857dcad90d97a913cfc5a6731a163909c61d2fb6
Christian G. <70849096+cmtg@users.noreply.github.com>
2024-11-17 11:51:20 -0300

    Added generic_usernames to simple.py

--- gambit/algorithms/simple.py
+++ gambit/algorithms/simple.py
@@ -9,13 +9,15 @@ def get_email_base(s):
     return s
 
 
-def compare_rows(idx1, idx2, row1, row2):
+def compare_rows(idx1, idx2, row1, row2, generic_usernames=['git','github','root','admin','administrator','system','test','guest','anonymous']):
     tresh = 3
 
     if (len(str(row1.name)) >= tresh) and (len(str(row2.name)) >= tresh) and (row1.name == row2.name):
         return True
     elif (len(str(row1.email_base)) >= tresh) and \
          (len(str(row2.email_base)) >= tresh) and \
+         (str(row1.email_base) not in generic_usernames) and \
+         (str(row2.email_base) not in generic_usernames) and \
          (row1.email_base == row2.email_base):
         return True
     else:
@@ -25,7 +27,7 @@ def compare_rows(idx1, idx2, row1, row2):
 # -------------------------------------------------------------------
 
 
-def simple(authors):
+def simple(authors, generic_usernames=['git','github','root','admin','administrator','system','test','guest','anonymous']):
     authors.reset_index(inplace=True, drop=True)
 
     authors['name'] = authors['alias_name']
@@ -42,7 +44,7 @@ def simple(authors):
                 next_id += 1
 
             for idx2, row2 in authors[idx1+1:].iterrows():
-                if compare_rows(idx1, idx2, row1, row2):
+                if compare_rows(idx1, idx2, row1, row2, generic_usernames=generic_usernames):
                     if pd.notnull(authors.loc[idx1, 'author_id']) and pd.notnull(authors.loc[idx2, 'author_id']):
                         min_id = min(authors.loc[idx1, 'author_id'], authors.loc[idx2, 'author_id'])
                         authors.loc[authors.author_id == authors.loc[idx1, 'author_id'], 'author_id'] = min_id
